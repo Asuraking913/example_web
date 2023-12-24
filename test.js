fd467cddc3854d28ffc87069c3758e4291da575d
Asura king913 <israelshedrack913@gmail.com>
2023-12-24 17:32:37 +0100

    Learnt new things

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
 //DOM manipulation
 
-const title = document.querySelector('#heading');
-title.style.padding = '3em'
+const ul = document.querySelector('ul');
+
+const new_div = document.createElement('li');
+
+new_div.innerText = 'Asura';
+
+ul.append(new_div);
+
+const li = document.querySelectorAll('li');
+
+// console.log(ul.parentNode);
+// console.log(ul.parentElement);
