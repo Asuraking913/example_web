d8989ea4507e3ff70fd216ac5172f8e5631c80c0
Asura king913 <israelshedrack913@gmail.com>
2023-12-25 19:25:52 +0100

    Quote Generator

--- test.js
+++ test.js
@@ -1,13 +1,35 @@
-//Event listeners
-const hiddenContent = document.querySelector(".hidden");
-const revealBtn = document.querySelector(".but2");
+const quote = [
+  {
+    quote: "The only thing we have to fear is fear itself.",
+    person: "- Franklin D. Roosevelt",
+  },
+  {
+    quote:
+      "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    person: -"- Martin Luther King Jr",
+  },
+  {
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    person: "- Ralph Waldo Emerson",
+  },
+  {
+    quote: "The only impossible journey is the one you never begin.",
+    person: "- Tony Robbins",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    person: "- Nelson Mandela",
+  },
+];
 
-function revealContent() {
-  if (hiddenContent.classList.contains("hidden")) {
-    hiddenContent.classList.remove("hidden");
-  } else {
-    hiddenContent.classList.add("hidden");
-  }
-}
+let button = document.querySelector("button");
+let texQuote = document.querySelector(".quote");
+let author = document.querySelector("h3");
 
-revealBtn.addEventListener("click", revealContent);
+button.addEventListener("click", function () {
+  let random = Math.floor(Math.random() * quote.length);
+  texQuote.innerText = quote[random].quote;
+  author.innerText = quote[random].person;
+});
--- test1.js
+++ test1.js
@@ -1,11 +1,3 @@
-function Car (maxspeed, driver) {
-    this.driver = driver;
-    this.maxspeed = maxspeed;
-    this.displayLog = function displayLog() {
-        console.log('Welcome, ' + this.driver + ' ' + 'you have a maxspeed of ' + this.maxspeed)
-    }
-}
+var list = ["the", "man", "is", "good"];
 
-var myCar = new Car(120, 'Asura King');
-
-console.log(myCar.displayLog())
+console.log(Math.random());
