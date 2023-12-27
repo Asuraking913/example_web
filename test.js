b8cc52118d363ee3ebd603353382f901936edb3f
Asura king913 <israelshedrack913@gmail.com>
2023-12-27 23:36:26 +0100

    new changes

---
+++ test.js
@@ -0,0 +1,7 @@
+const playBtn = document.getElementById("play");
+const stopBtn = document.getElementById("stop");
+const timer = document.getElementById("timer");
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
