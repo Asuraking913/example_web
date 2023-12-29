e3bee1ba3d2a4150ed36b3270cf64638fe0c1ee0
Asura king913 <israelshedrack913@gmail.com>
2023-12-29 23:55:42 +0100

    Not finished

--- test.js
+++ test.js
@@ -1,39 +1,31 @@
 const container = document.getElementById("container");
-const button = document.getElementById("button");
+const addBtn = document.getElementById("button");
 const inputText = document.getElementById("text-input");
-const newDiv = document.getElementsByClassName("cont3");
-// const newButton1 = document.getElementsByClassName("check");
-// const newButton2 = document.getElementsByClassName("delete");
 
-button.addEventListener("click", function () {
+addBtn.addEventListener("click", function () {
   if (inputText.value === "") {
-    alert("You did not input any value");
+    awlert("you have not inputed any value");
   } else {
     let newCont = document.createElement("div");
+    let newList = document.createElement("li");
+
     newCont.classList.add("cont3");
-    let newText = document.createElement("p");
-    newText.innerText = inputText.value;
-    newCont.appendChild(newText);
+    newCont.appendChild(newList);
+
+    newList.innerText = inputText.value;
     let button1 = document.createElement("button");
-    button1.classList.add("check");
-    button2.classList.add("delete");
     let button2 = document.createElement("button");
     let icon1 = document.createElement("i");
     let icon2 = document.createElement("i");
+
     icon1.innerHTML = '<i class="fa-solid fa-check"></i>';
     icon2.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
     button1.appendChild(icon1);
     button2.appendChild(icon2);
-    newCont.appendChild(button1);
-    newCont.appendChild(button2);
+    newList.appendChild(button1);
+    newList.appendChild(button2);
 
     container.appendChild(newCont);
   }
-  inputText.value = "";
 });
-
-// window.addEventListener("click", function (e) {
-//   if (e.target.className == "check") {
-//     e.target.paren
-//   }
-// });
