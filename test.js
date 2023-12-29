75932a884f64f49f29168c6c9179475f206ca902
Asura king913 <israelshedrack913@gmail.com>
2023-12-30 00:41:23 +0100

    Not finished

--- test.js
+++ test.js
@@ -4,17 +4,21 @@ const inputText = document.getElementById("text-input");
 
 addBtn.addEventListener("click", function () {
   if (inputText.value === "") {
-    awlert("you have not inputed any value");
+    alert("you have not inputed any value");
   } else {
     let newCont = document.createElement("div");
     let newList = document.createElement("li");
+    let text = document.createElement("p");
 
     newCont.classList.add("cont3");
     newCont.appendChild(newList);
+    text.innerText = inputText.value;
+    newList.appendChild(text);
 
-    newList.innerText = inputText.value;
     let button1 = document.createElement("button");
     let button2 = document.createElement("button");
+    button1.classList.add("check");
+    button2.classList.add("delete");
     let icon1 = document.createElement("i");
     let icon2 = document.createElement("i");
 
@@ -27,5 +31,16 @@ addBtn.addEventListener("click", function () {
     newList.appendChild(button2);
 
     container.appendChild(newCont);
+
+    button1.addEventListener("click", function (e) {
+      e.target.parentElement.parentElement.previousSibling.style.textDecoration =
+        "line-through";
+    });
+
+    button2.addEventListener("click", function (e) {
+      e.target.parentElement.parentElement.parentElement.parentElement.remove();
+    });
+
+    inputText.value = "";
   }
 });
