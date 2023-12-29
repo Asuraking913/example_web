0673ed67f82b826fc40ad562a25bdba74e58ffef
Asura king913 <israelshedrack913@gmail.com>
2023-12-29 07:22:21 +0100

    New changes 95%

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const container = document.getElementById("container");
 const button = document.getElementById("button");
 const inputText = document.getElementById("text-input");
 const newDiv = document.getElementsByClassName("cont3");
+// const newButton1 = document.getElementsByClassName("check");
+// const newButton2 = document.getElementsByClassName("delete");
 
 button.addEventListener("click", function () {
   if (inputText.value === "") {
@@ -13,11 +15,13 @@ button.addEventListener("click", function () {
     newText.innerText = inputText.value;
     newCont.appendChild(newText);
     let button1 = document.createElement("button");
+    button1.classList.add("check");
+    button2.classList.add("delete");
     let button2 = document.createElement("button");
     let icon1 = document.createElement("i");
     let icon2 = document.createElement("i");
-    icon1.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    icon2.innerHTML = '<i class="fa-solid fa-check"></i>';
+    icon1.innerHTML = '<i class="fa-solid fa-check"></i>';
+    icon2.innerHTML = '<i class="fa-solid fa-trash"></i>';
     button1.appendChild(icon1);
     button2.appendChild(icon2);
     newCont.appendChild(button1);
@@ -28,6 +32,8 @@ button.addEventListener("click", function () {
   inputText.value = "";
 });
 
-// newDiv.addEventListener("click", function(e) {
-//     if
-// })kjhkj
+// window.addEventListener("click", function (e) {
+//   if (e.target.className == "check") {
+//     e.target.paren
+//   }
+// });
